e46cce886884b1f023935b3413715e2951d2e587
Shani Nuyts <93946464+USFBull00@users.noreply.github.com>
2023-01-16 01:18:44 +0000

    main

---
+++ Python/Finding_An_A.py
@@ -0,0 +1,10 @@
+string = input()
+top = len(string)
+#print(top)
+
+#print(string[top])
+for i in range(0,top):
+    #print(string[i])
+    if string[i] == 'a':
+        print(string[i:top])
+        break
