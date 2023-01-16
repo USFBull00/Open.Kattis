0ff862d35b5644a242c4e3e7a4d070b12f90f8d2
Shani Nuyts <93946464+USFBull00@users.noreply.github.com>
2023-01-16 01:15:30 +0000

    main

---
+++ Python/Espresso.py
@@ -0,0 +1,22 @@
+n, s = input().split()
+students = int(n)
+size = int(s)
+
+refills = 0
+left_over = size
+
+for i in range(students):
+    order = input()
+    shots = int(order[0])
+    needed = 0
+    needed += shots
+    if order[-1] =='L':
+        needed += 1
+    if left_over >= needed:
+        left_over -= needed
+    elif left_over < needed:
+        refills += 1
+        left_over = size - needed
+    #print(order, needed, left_over, refills)
+
+print(refills)
